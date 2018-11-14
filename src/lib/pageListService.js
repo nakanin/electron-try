cd03f201587d21f87cddebdc2fcbce515c771f20
Naotaka Nakanishi <nakanishi-naotaka@osscat.net>
2018-11-14 11:29:45 +0900

    bodyタグのテキストで差分を検出するようにした

--- src/lib/pageListService.js
+++ src/lib/pageListService.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const request = require('request-promise-native');
 const crypto = require('crypto')
+const cheerio = require('cheerio')
 
 const filename = 'pageList.json'
 const requestConfigFilename = 'requestConfig.json'
@@ -15,6 +16,36 @@ const md5hex = (str) => {
   const md5 = crypto.createHash('md5')
   return md5.update(str, 'binary').digest('hex')
 };
+
+const getText = (html) => {
+  const $ = cheerio.load(html);
+  return $('body').text().replace(/\s\s+/g, ' ');
+}
+
+const saveText = (url, text) => {
+  const specialChars = /\:|\?|\.|"|<|>|\|/g;   //使用できない特殊文字
+  const slash        = /\//g;                  //単一のスラッシュ
+  const spaces       = /\s\s+/g;               //連続したスペース
+  const hyphens      = /-+/g;                  //連続したハイフン
+  const filename = url
+    .replace(specialChars, '-')
+    .replace(slash, '-')
+    .replace(spaces, '-')
+    .replace(hyphens, '-')
+    .substr(0, 50);
+
+  const dir = 'raw/';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  const filepath = dir + filename + '.txt';
+  if (fs.existsSync(filepath)) {
+    const backupPath = filepath.replace(/\.txt$/, '_old.txt');
+    fs.renameSync(filepath, backupPath);
+  }
+  fs.writeFileSync(filepath, text, 'utf-8');
+}
 
 const writeFile = (list) => {
   fs.writeFileSync(filename, JSON.stringify(list, null, '    '), 'utf-8');
@@ -57,12 +88,15 @@ const pageListService = {
   checkUpdate: function () {
     let requests = [];
     let config = getRequestConfig();
-    console.log(config);
+
     this.getList().forEach(page => {
       requests.push(
         request.get(page.url, config)
-          .then(data => {
-            let hash = md5hex(data);
+          .then(html => {
+            const text = getText(html);
+            saveText(page.url, text);
+
+            const hash = md5hex(text);
             if (hash === page.hash) {
               page.status = STATUS_NO_CHANGE;
             } else {
@@ -84,7 +118,7 @@ const pageListService = {
     return Promise.all(requests)
       .then(responses => {
         writeFile(responses);
-        return(responses);
+        return responses;
       });
   }
 };
